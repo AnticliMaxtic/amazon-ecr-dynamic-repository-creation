f7d9b72a6a1a6808c1ba920ff1a6358ce6faae38
Max Christy <max.christy@ptxag.com>
2025-08-07 16:32:48 -0500

    refactor: use latest syntax for data source aws_region

--- ecr/locals.tf
+++ ecr/locals.tf
@@ -1,6 +1,6 @@
 locals {
   common_tags        = merge({ "managed-by" = "terraform" })
-  current_region     = data.aws_region.current.name
+  current_region     = data.aws_region.current.region
   current_account_id = data.aws_caller_identity.current.account_id
   full_name          = "dynamic-create-ecr-repo-${local.current_region}"
   repo_tags          = merge(var.REPO_TAGS)
